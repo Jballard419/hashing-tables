444bd02f61afa6616cdfe84eba504c72d51d6fba
James Ballard <j.lb4@live.com>
2017-02-12 16:33:53 -0600

    basic done need to modify main

---
+++ Ballard_Lab03/hash_table.h
@@ -0,0 +1,47 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <iostream>
+enum Type_of_hash { quadtric,  double_hash };
+
+template <typename T>
+class hash_table
+{
+  public:
+
+    hash_table(int prime_num);
+
+    hash_table(int prime_num, int second_prime_num);
+
+    ~hash_table();
+
+    void insert(const T val);
+
+    void remove(const T val);
+
+
+    int search(const T val);
+
+    int hash_quadtric(const T val, bool insert);
+    int hash_double(const T val, bool insert);
+
+
+    void print();
+
+  private:
+
+    <T>* hash_map;
+    bool* has_been_used;
+    int size_of_map;
+    Type_of_hash version;
+    int second_prime;
+
+
+
+
+
+
+
+};
+#include "hash_table.hpp"
+#endif
